e084f5972b908569f28fe6f968671b15aefedcbb
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2019-11-28 13:52:27 -0300

    workshop app'

--- android/app/src/main/java/com/workshopcodepush/MainApplication.java
+++ android/app/src/main/java/com/workshopcodepush/MainApplication.java
@@ -30,7 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          packages.add(new CodePush("deployment-key-here", MainApplication.this, BuildConfig.DEBUG));
+          packages.add(new CodePush(BuildConfig.CODEPUSH_KEY, MainApplication.this, BuildConfig.DEBUG));
           return packages;
         }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,98 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import CodePush from 'react-native-code-push';
 
 import styles from './styles';
 
 function App() {
+  const [hasUpdate, setHasUpdate] = useState(false);
+  const [mandatory, setMandatory] = useState(false);
+  const [status, setStatus] = useState('');
+
+  useEffect(() => {
+    async function checkPackages() {
+      const newUpdate = await CodePush.checkForUpdate();
+
+      if (!newUpdate) return;
+
+      if (newUpdate.isMandatory) {
+        setMandatory(true);
+      } else {
+        CodePush.sync();
+      }
+    }
+
+    checkPackages();
+  }, []);
+
+  async function handleCheckForUpdate() {
+    const newUpdate = await CodePush.checkForUpdate();
+
+    setHasUpdate(newUpdate);
+  }
+
+  async function handleUpdate() {
+    await CodePush.sync(
+      { updateDialog: false, installMode: CodePush.InstallMode.IMMEDIATE },
+      statusSync => {
+        switch (statusSync) {
+          case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+            setStatus('Verificando att');
+            break;
+          case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+            setStatus('Baixando Pacotes');
+            break;
+          case CodePush.SyncStatus.INSTALLING_UPDATE:
+            setStatus('Instalando');
+            break;
+          default:
+            setStatus('atualizado');
+        }
+      },
+      () => CodePush.allowRestart()
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Workshop CodePush</Text>
+      <Text style={styles.title}>Workshop CodePush (obrigatorio)</Text>
+
+      {!mandatory && (
+        <>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleCheckForUpdate}>
+            <Text>Verificar</Text>
+          </TouchableOpacity>
+
+          {!hasUpdate ? (
+            <Text>Ta atualizado</Text>
+          ) : (
+            <TouchableOpacity style={styles.button} onPress={handleUpdate}>
+              <Text>Atualizar para {hasUpdate.label}</Text>
+            </TouchableOpacity>
+          )}
+        </>
+      )}
+
+      <Text>{status}</Text>
+
+      {mandatory && (
+        <>
+          <Text style={styles.title}>Obrigatorio</Text>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleCheckForUpdate}>
+            <Text>Verificar</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 }
 
-export default CodePush()(App);
+export default CodePush({ checkFrequency: CodePush.CheckFrequency.MANUAL })(
+  App
+);
